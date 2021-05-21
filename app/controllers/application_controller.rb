db84cf4a2b0aeb13650e8aae45b61444916457ad
DAIKINKO <mmxii.no.xxii.n.et.d.secvlorvm@gmail.com>
2021-05-21 15:50:49 +0900

    ユーザ認証機能_編集画面表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
+  # m210521_1行目では、ログインしていない輩は自動でログイン画面に導くようにしてるよ
+  # 2行目では、デバイズコントローラが処理するときは、ピックアップするparamsに
+  # # nameも仲間にいれてあげてあげよーという処理をしてるよ
+  # サニタイザーはデバイズをインストールしているから使えるチートコードだよ
+  # 「自作カラムを仲間にいれてあげよう、ふっふっふ」という偉そうなやつだよ。
+  
+
+
 end
+
+
+
